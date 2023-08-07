c645d1678d8cb77ca802a2e4489c126ddc48580a
Supriya Maddu <117802859+SupriyaMaddu@users.noreply.github.com>
2023-08-07 20:39:42 +0530

    Add files via upload

---
+++ stringRepetition.py
@@ -0,0 +1,19 @@
+print("**")
+print("**")
+
+print("* " * 2)
+print("* " * 2)
+
+print("*")
+print("**")
+
+print("* ")
+print("* " * 2)
+
+a = input()
+word = a + ' '
+print(word * 3) 
+
+
+word = input()
+print("* " * 3 + word + " *" * 3)
